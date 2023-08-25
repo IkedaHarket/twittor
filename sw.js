e5fe83e4e1451f6fdd2cc0a30cb54c0192d86973
Ikeda Harket <sebaaignacio111@gmail.com>
2023-08-25 12:37:14 -0400

    Modificacion ruta sw

--- sw.js
+++ sw.js
@@ -5,23 +5,23 @@ const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
-    '/js/sw-utils.js',
+    // '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js',
 ];
 
 const APP_SHELL_INMUTABLE = [
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css',
-    '/css/animate.css',
-    '/js/libs/jquery.js',
+    'css/animate.css',
+    'js/libs/jquery.js',
 ]
 
 self.addEventListener('install', (e) =>{
